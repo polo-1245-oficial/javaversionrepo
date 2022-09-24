1a57e845ea119dd52de7b8da4a264d1e231228aa
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2022-09-24 23:23:20 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -3,4 +3,4 @@ mkdir -p /etc/apt/keyrings
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
 echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
 apt update
-apt install temurin-8-jdk
+apt install temurin-11-jdk
