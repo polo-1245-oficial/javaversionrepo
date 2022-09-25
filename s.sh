223eb2144a80591611f64969912af6b1b9ca54f7
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2022-09-25 09:32:24 +0200

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,23 @@
+sudo source /temp/bar.sh
+
+
+
+main() {
+    # Make sure that the progress bar is cleaned up when user presses ctrl+c
+    enable_trapping
+    # Create progress bar
+    setup_scroll_area
+    for i in {1..99}
+                `apt install -y wget apt-transport-https`
+    `mkdir -p /etc/apt/keyrings`
+    `wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc`
+    `echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list` 
+    `apt update -y`
+    `apt install temurin-8-jdk -y` 
+            draw_progress_bar $i
+
+    done
+    destroy_scroll_area
+}
+
+main
