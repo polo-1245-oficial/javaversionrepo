ba0bcdddefa93f2926d147ccc4036fac3b837281
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2022-09-25 09:13:21 +0200

    test

--- installer.sh
+++ installer.sh
@@ -1,20 +1,33 @@
-#FROM https://stackoverflow.com/a/36538247/14882381
+sudo source ./temp/bar.sh
+
+generate_some_output_and_sleep() {
+    apt install -y wget apt-transport-https
+    sleep 0.3
+    mkdir -p /etc/apt/keyrings
+    wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc 
+    echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list 
+    apt update -y 
+    apt install temurin-8-jdk -y 
+}
 
 
-sleep 20 & PID=$! 
+main() {
+    # Make sure that the progress bar is cleaned up when user presses ctrl+c
+    enable_trapping
+    # Create progress bar
+    setup_scroll_area
+    for i in {1..99}
+    do
+        if [ $i = 50 ]; then
+            echo "waiting for user input"
+            block_progress_bar $i
+            read -p "User input: "
+        else
+            generate_some_output_and_sleep
+            draw_progress_bar $i
+        fi
+    done
+    destroy_scroll_area
+}
 
-echo "Instalando Java $VER"
-printf "["
-while kill -0 $PID 2> /dev/null; do 
-    printf  "▓"
-    sleep 1
-done
-printf "] Listo!"
-
-apt install -y wget apt-transport-https &
-mkdir -p /etc/apt/keyrings &
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc &
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list &
-apt update -y &
-apt install temurin-8-jdk -y &
-command &
+main
