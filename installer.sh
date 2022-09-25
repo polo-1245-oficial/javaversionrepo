861d9c9de4cee5289e8068c65741a55b5610505e
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2022-09-25 09:30:57 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,13 +1,5 @@
 sudo source /temp/bar.sh
 
-generate_some_output_and_sleep() {
-    `apt install -y wget apt-transport-https`
-    `mkdir -p /etc/apt/keyrings`
-    `wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc`
-    `echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list` 
-    `apt update -y`
-    `apt install temurin-8-jdk -y` 
-}
 
 
 main() {
@@ -16,7 +8,12 @@ main() {
     # Create progress bar
     setup_scroll_area
     for i in {1..99}
-            generate_some_output_and_sleep
+                `apt install -y wget apt-transport-https`
+    `mkdir -p /etc/apt/keyrings`
+    `wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc`
+    `echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list` 
+    `apt update -y`
+    `apt install temurin-8-jdk -y` 
             draw_progress_bar $i
 
     done
